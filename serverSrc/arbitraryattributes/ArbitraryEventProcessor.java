869c2ccea1646009c5d014ecf1bac352fa241622
David Rolfe <drolfe@voltdb.com>
2021-11-23 19:01:44 +0000

    Added runtime jar files

--- serverSrc/arbitraryattributes/ArbitraryEventProcessor.java
+++ serverSrc/arbitraryattributes/ArbitraryEventProcessor.java
@@ -28,12 +28,46 @@ import java.math.BigDecimal;
 import org.voltdb.VoltTable;
 import org.voltdb.types.TimestampType;
 
+/**
+ * Interface that allows you to add or change business functionality at will. Each class that implements this gets a handle to
+ * EventReporterInterface, which is a set of methods in the parent stored procedure for storing data and raising alerts. 
+ *
+ */
 public interface ArbitraryEventProcessor {
     
+    /**
+     * 
+     * A new event has arrived, and we need to decide what to do with it. We can use the methods exposed by 
+     * EventReporterInterface to do this. This method is called once for each ArbitraryEventProcessor.
+     * 
+     * @param theReporter
+     * @param cardid
+     * @param txn_time
+     * @param txn_id
+     * @param txn_amount
+     * @param txn_kind
+     * @param txn_tag1
+     * @param txn_tag1_value
+     * @param txn_tag2
+     * @param txn_tag2_value
+     * @param txn_tag3
+     * @param txn_tag3_value
+     * @param txn_tag4
+     * @param txn_tag4_value
+     * @return number of records created
+     */
     public int noteDetailsOfEvent(EventReporterInterface theReporter, String cardid, TimestampType txn_time, String txn_id, BigDecimal txn_amount,
             String txn_kind, String txn_tag1, long txn_tag1_value, String txn_tag2, long txn_tag2_value,
             String txn_tag3, long txn_tag3_value, String txn_tag4, long txn_tag4_value);
 
+    /**
+     *  
+     * 
+     * @param theReporter
+     * @param totalsTable
+     * @param totalsByMinuteTable
+     * @return number of alerts raised
+     */
     public int raiseAlertsIfNeeded(EventReporterInterface theReporter, VoltTable totalsTable, VoltTable totalsByMinuteTable);
 
 }
